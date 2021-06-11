5f6aee835cc33c961a933fbf79fe3f3ffe062731
Adam Tyson <code@adamltyson.com>
2021-06-11 09:55:58 +0100

    remove old docstrings

--- brainglobe_napari_io/cellfinder/writer_xml.py
+++ brainglobe_napari_io/cellfinder/writer_xml.py
@@ -9,16 +9,6 @@ from .utils import convert_layer_to_cells
 
 @napari_hook_implementation(specname="napari_get_writer")
 def cellfinder_write_xml(path: str):
-    """Returns a h5 Napari project writer if the path file format is h5.
-    Parameters
-    ----------
-    path: str
-        Napari h5 project file
-    Returns
-    -------
-    Callable or None
-        Napari h5 project file writer if the path file extension is correct
-    """
     if isinstance(path, str) and path.endswith(".xml"):
         return write_points_to_xml
     else:
@@ -28,18 +18,6 @@ def cellfinder_write_xml(path: str):
 def write_points_to_xml(
     path: str, layer_data: List[Tuple[Any, Dict, str]]
 ) -> str:
-    """Returns a list of LayerData tuples from the project file required by Napari.
-    Parameters
-    ----------
-    path: str
-        xml output directory
-    layer_data: list[tuple[numpy array, dict, str]]
-        List of LayerData tuples produced by Napari IO writer
-    Returns
-    -------
-    str
-        Final output file path
-    """
     cells_to_save = []
     for layer in layer_data:
         data, state, type = layer
