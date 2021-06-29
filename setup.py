ad34d936840a6dfd8d9c5d2dc82d8ca1fe26d365
Adam Tyson <code@adamltyson.com>
2021-06-29 12:27:39 +0100

    Bump version: 0.1.3-rc0 → 0.1.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name="brainglobe-napari-io",
-    version="0.1.3-rc0",
+    version="0.1.3",
     author="Adam Tyson",
     author_email="code@adamltyson.com",
     license="BSD-3-Clause",
