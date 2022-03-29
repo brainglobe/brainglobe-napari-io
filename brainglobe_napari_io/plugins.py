534926bc49e1cdce30001d0059b9ea69eef85a9a
David Stansby <dstansby@gmail.com>
2022-03-29 10:20:04 +0100

    Add tests for XML writer (#13)
    * Refactor XML writer
    
    * Remove single writer

--- brainglobe_napari_io/plugins.py
+++ brainglobe_napari_io/plugins.py
@@ -3,7 +3,7 @@ from brainglobe_napari_io.cellfinder.reader_dir import cellfinder_read_dir
 
 from brainglobe_napari_io.brainreg.reader_dir import brainreg_read_dir
 
-from brainglobe_napari_io.cellfinder.writer_multiple_xml import (
+from brainglobe_napari_io.cellfinder.writer_xml import (
+    cellfinder_write_xml,
     cellfinder_write_multiple_xml,
 )
-from brainglobe_napari_io.cellfinder.writer_xml import cellfinder_write_xml
