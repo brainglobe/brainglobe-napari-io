fc2e0d18d97da372ba44ae7c8e190b42c8194ffc
David Stansby <dstansby@gmail.com>
2023-02-07 11:33:01 +0000

    Make pre-commit happy

--- brainglobe_napari_io/brainreg/reader_dir_standard_space.py
+++ brainglobe_napari_io/brainreg/reader_dir_standard_space.py
@@ -50,7 +50,9 @@ def reader_function(path: os.PathLike) -> List[LayerDataTuple]:
         A list of LayerData tuples where each tuple in the list contains
         (data, metadata, layer_type), where data is a numpy array, metadata is
         a dict of keyword arguments for the corresponding viewer.add_* method
-        in napari, and layer_type is a lower-case string naming the type of layer.
+        in napari, and layer_type is a lower-case string naming the type of
+        layer.
+
         Both "meta", and "layer_type" are optional. napari will default to
         layer_type=="image" if not provided
     """
