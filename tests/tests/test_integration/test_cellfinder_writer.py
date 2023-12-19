997625096d212aa7b606b0f24366bf1d72c34d83
Adam Tyson <code@adamltyson.com>
2023-12-19 14:34:08 +0000

    Expand cellfinder xml writer tests

--- tests/tests/test_integration/test_cellfinder_writer.py
+++ tests/tests/test_integration/test_cellfinder_writer.py
@@ -1,5 +1,7 @@
 import pathlib
 
+import numpy as np
+from brainglobe_utils.cells.cells import Cell
 from brainglobe_utils.IO.cells import get_cells
 
 from brainglobe_napari_io.cellfinder import reader_xml, writer_xml
@@ -13,7 +15,7 @@ def test_xml_roundrip(tmp_path):
     layers = reader_xml.xml_reader(validate_xml_file)
     assert len(layers) == 2
 
-    test_xml_path = str(tmp_path / "multiple.xml")
+    test_xml_path = str(tmp_path / "points.xml")
     paths = writer_xml.write_multiple_points_to_xml(test_xml_path, layers)
     assert len(paths) == 1
     assert isinstance(paths[0], str)
@@ -24,3 +26,42 @@ def test_xml_roundrip(tmp_path):
     assert len(cells_test) == len(cells_validate)
     assert cells_test[0] == cells_validate[0]
     assert cells_test[-1] == cells_validate[-1]
+
+
+def test_xml_write_no_metadata(tmp_path):
+    xml_path = str(tmp_path / "points.xml")
+    points = (
+        np.random.random((10, 3)),
+        {
+            "name": "test",
+            "size": 1,
+            "n_dimensional": True,
+            "opacity": 1,
+            "symbol": "ring",
+            "face_color": "lightskyblue",
+            "metadata": {},
+        },
+        "points",
+    )
+    writer_xml.write_multiple_points_to_xml(xml_path, [points])
+    cells = get_cells(xml_path)
+    assert len(cells) == 10
+    assert cells[0].type == Cell.UNKNOWN
+
+
+def test_xml_write_no_points(tmp_path):
+    xml_path = str(tmp_path / "points.xml")
+    points = (
+        np.array([]),
+        {
+            "name": "test",
+            "size": 1,
+            "n_dimensional": True,
+            "opacity": 1,
+            "symbol": "ring",
+            "face_color": "lightskyblue",
+            "metadata": {},
+        },
+        "points",
+    )
+    assert writer_xml.write_multiple_points_to_xml(xml_path, [points]) == []
