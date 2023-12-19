32f8eab416c2dcee0fa828b9752d73170af5e005
Adam Tyson <code@adamltyson.com>
2023-12-19 14:41:44 +0000

    Test loading other files

--- tests/tests/test_integration/test_cellfinder_reader.py
+++ tests/tests/test_integration/test_cellfinder_reader.py
@@ -6,13 +6,20 @@ from brainglobe_utils.cells.cells import Cell
 from brainglobe_napari_io.cellfinder import reader_xml
 
 test_data_dir = pathlib.Path(__file__).parent.parent.parent / "data"
+xml_file = test_data_dir / "cell_classification.xml"
+broken_xml = test_data_dir / "broken_xml.xml"
+xml_with_incorrect_root_tag = test_data_dir / "incorrect_tag.xml"
+
+
+def test_is_cellfinder_xml():
+    assert reader_xml.is_cellfinder_xml(xml_file)
+    assert not reader_xml.is_cellfinder_xml(__file__)
+    assert not reader_xml.is_cellfinder_xml(broken_xml)
+    assert not reader_xml.is_cellfinder_xml(broken_xml)
+    assert not reader_xml.is_cellfinder_xml(xml_with_incorrect_root_tag)
 
 
 def test_reader_xml():
-    # Basic smoke tests for the XML reader
-    xml_file = test_data_dir / "cell_classification.xml"
-    assert reader_xml.is_cellfinder_xml(xml_file)
-    assert not reader_xml.is_cellfinder_xml(__file__)
     assert reader_xml.cellfinder_read_xml(str(xml_file.resolve())) is not None
     layers = reader_xml.xml_reader(xml_file)
 
