5f10a77d8d8add300e6cc3742f1df6aa6bf80840
Adam Tyson <code@adamltyson.com>
2023-12-19 12:06:28 +0000

    Add more assertions to the cellfinder xml reader test

--- tests/test_cellfinder_reader.py
+++ tests/test_cellfinder_reader.py
@@ -1,6 +1,7 @@
 import pathlib
 
 import numpy as np
+from brainglobe_utils.cells.cells import Cell
 
 from brainglobe_napari_io.cellfinder import reader_xml
 
@@ -15,8 +16,21 @@ def test_reader_xml():
     assert reader_xml.cellfinder_read_xml(str(xml_file.resolve())) is not None
     layers = reader_xml.xml_reader(xml_file)
 
+    assert len(layers) == 2
+
     for layer in layers:
         assert len(layer) == 3
         assert isinstance(layer[0], np.ndarray)
         assert isinstance(layer[1], dict)
         assert isinstance(layer[2], str)
+
+        assert layer[2] == "points"
+
+    assert layers[0][1]["name"] == "Non cells"
+    assert layers[1][1]["name"] == "Cells"
+
+    assert layers[0][1]["metadata"]["point_type"] == Cell.UNKNOWN
+    assert layers[1][1]["metadata"]["point_type"] == Cell.CELL
+
+    assert len(layers[0][0]) == 22
+    assert len(layers[1][0]) == 103
