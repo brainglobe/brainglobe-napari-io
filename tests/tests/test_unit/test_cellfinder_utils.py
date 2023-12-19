df82a81825723047a3762332acd167034c28b61e
Adam Tyson <code@adamltyson.com>
2023-12-19 14:57:25 +0000

    Add test for layer naming

--- tests/tests/test_unit/test_cellfinder_utils.py
+++ tests/tests/test_unit/test_cellfinder_utils.py
@@ -1,12 +1,14 @@
-import pytest
 import pathlib
+
 import numpy as np
+import pytest
 from brainglobe_utils.cells.cells import Cell
 
 from brainglobe_napari_io.cellfinder import utils
 
 test_data_dir = pathlib.Path(__file__).parent.parent.parent / "data"
 xml_file = test_data_dir / "cell_classification.xml"
+
 
 @pytest.fixture
 def cell_layers():
@@ -17,8 +19,25 @@ def cell_layers():
         1,
         "disk",
         "lightgoldenrodyellow",
-        "lightskyblue", )
+        "lightskyblue",
+    )
     return layers
+
+
+@pytest.fixture
+def cell_layers_channel_6():
+    layers = utils.load_cells(
+        [],
+        xml_file,
+        1,
+        1,
+        "disk",
+        "lightgoldenrodyellow",
+        "lightskyblue",
+        channel=6,
+    )
+    return layers
+
 
 def test_load_cells(cell_layers):
     assert len(cell_layers) == 2
@@ -39,6 +58,13 @@ def test_load_cells(cell_layers):
 
     assert len(cell_layers[0][0]) == 22
     assert len(cell_layers[1][0]) == 103
+
+
+def test_load_cells_with_channel(cell_layers_channel_6):
+    assert len(cell_layers_channel_6) == 2
+    assert cell_layers_channel_6[0][1]["name"] == "channel_6: Non cells"
+    assert cell_layers_channel_6[1][1]["name"] == "channel_6: Cells"
+
 
 def test_convert_layer_to_cells(cell_layers):
     cells = utils.convert_layer_to_cells(cell_layers[0][0])
