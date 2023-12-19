74c3587a19271245e1ae31e52d16db8ec1f04df6
Adam Tyson <code@adamltyson.com>
2023-12-19 14:13:20 +0000

    Add more assertions for writer test

--- tests/tests/test_integration/test_cellfinder_writer.py
+++ tests/tests/test_integration/test_cellfinder_writer.py
@@ -1,19 +1,26 @@
 import pathlib
 
+from brainglobe_utils.IO.cells import get_cells
+
 from brainglobe_napari_io.cellfinder import reader_xml, writer_xml
 
 test_data_dir = pathlib.Path(__file__).parent.parent.parent / "data"
 
 
-def test_xml_roundrip(tmpdir):
+def test_xml_roundrip(tmp_path):
     # Check that a read in XML file can also be written back out
-    xml_file = test_data_dir / "cell_classification.xml"
-    layers = reader_xml.xml_reader(xml_file)
+    validate_xml_file = test_data_dir / "cell_classification.xml"
+    layers = reader_xml.xml_reader(validate_xml_file)
     assert len(layers) == 2
 
-    paths = writer_xml.write_multiple_points_to_xml(
-        str(tmpdir / "multiple.xml"), layers
-    )
+    test_xml_path = str(tmp_path / "multiple.xml")
+    paths = writer_xml.write_multiple_points_to_xml(test_xml_path, layers)
     assert len(paths) == 1
     assert isinstance(paths[0], str)
     assert reader_xml.is_cellfinder_xml(paths[0])
+
+    cells_validate = get_cells(str(validate_xml_file))
+    cells_test = get_cells(test_xml_path)
+    assert len(cells_test) == len(cells_validate)
+    assert cells_test[0] == cells_validate[0]
+    assert cells_test[-1] == cells_validate[-1]
