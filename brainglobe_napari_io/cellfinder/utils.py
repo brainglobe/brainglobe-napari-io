6343f304d0db6ac559444ca380d17d95507a68cd
Matt Einhorn <matt@einhorn.dev>
2025-07-07 19:03:22 -0400

    Remove need for dataframes.

--- brainglobe_napari_io/cellfinder/utils.py
+++ brainglobe_napari_io/cellfinder/utils.py
@@ -1,42 +1,34 @@
 from pathlib import Path
-from typing import List, Tuple
 
 import numpy as np
 import pandas as pd
 from brainglobe_utils.cells.cells import Cell
-from brainglobe_utils.IO.cells import cells_to_dataframe, get_cells
+from brainglobe_utils.IO.cells import get_cells
 from napari.types import LayerDataTuple
 
 
-def cells_df_as_np(
-    cells_df: pd.DataFrame,
-    new_order: List[int] = [2, 1, 0],
-    type_column: str = "type",
-) -> np.ndarray:
-    cells_df = cells_df.drop(columns=[type_column])
-    cells = cells_df[cells_df.columns[new_order]]
-    cells = cells.to_numpy()
-    return cells
-
-
-def cells_metadata_to_df(cells: list[Cell], type: int) -> pd.DataFrame:
+def _cells_metadata_to_df(cells: list[Cell], type: int) -> pd.DataFrame:
     return pd.DataFrame([c.metadata for c in cells if c.type == type])
 
 
-def get_cell_arrays(all_cells: list[Cell]) -> Tuple[np.ndarray, np.ndarray]:
-    df = cells_to_dataframe(all_cells)
+def get_cell_arrays(all_cells: list[Cell]) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Returns two Nx3 and Mx3 arrays with the z,y,x position of the cells that
+    are "UNKNOWN", and "CELL", respectively.
+    """
+    non_cells = [c for c in all_cells if c.type == Cell.UNKNOWN]
+    cells = [c for c in all_cells if c.type == Cell.CELL]
 
-    non_cells = df[df["type"] == Cell.UNKNOWN]
-    cells = df[df["type"] == Cell.CELL]
+    # napari expect z, y, x order
+    non_cells_pos = np.array([(c.z, c.y, c.z) for c in non_cells])
+    cells_pos = np.array([(c.z, c.y, c.z) for c in cells])
 
-    cells = cells_df_as_np(cells)
-    non_cells = cells_df_as_np(non_cells)
-    return cells, non_cells
+    return cells_pos, non_cells_pos
 
 
 def convert_layer_to_cells(
     layer_data, cells: bool = True, features: pd.DataFrame | None = None
-) -> List[Cell]:
+) -> list[Cell]:
     cells_to_save = []
     if cells:
         cell_type = Cell.CELL
@@ -57,7 +49,7 @@ def convert_layer_to_cells(
 
 
 def load_cells(
-    layers: List[LayerDataTuple],
+    layers: list[LayerDataTuple],
     classified_cells_path: Path,
     point_size: int,
     opacity: float,
@@ -65,11 +57,12 @@ def load_cells(
     cell_color: str,
     non_cell_color: str,
     channel=None,
-) -> List[LayerDataTuple]:
+) -> list[LayerDataTuple]:
     all_cells = get_cells(str(classified_cells_path), cells_only=False)
     cells, non_cells = get_cell_arrays(all_cells)
-    cells_metadata = cells_metadata_to_df(all_cells, Cell.CELL)
-    non_cells_metadata = cells_metadata_to_df(all_cells, Cell.UNKNOWN)
+    # napari accepts arbitrary features as a data frame
+    cells_metadata = _cells_metadata_to_df(all_cells, Cell.CELL)
+    non_cells_metadata = _cells_metadata_to_df(all_cells, Cell.UNKNOWN)
 
     if channel is not None:
         channel_base = f"channel_{channel}: "
